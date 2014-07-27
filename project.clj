d3260068a096eeb9f038e6bd8815c193ddfa5bf3
Mayur Jadhav <mayur@helpshift.com>
2014-07-27 23:25:51 +0530

    Add a function to convert xml file to clojure map format

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [cheshire "2.0.6"]
+                 [org.clojure/data.xml "0.0.7"]])
--- src/charles_clj/core.clj
+++ src/charles_clj/core.clj
@@ -1,6 +1,39 @@
-(ns charles-clj.core)
+(ns ^{:doc "Core functionality"
+      :author "Mayur Jadhav <mj.jadhav13@gmail.com>"}
+  charles-clj.core
+  (:require [clojure.java.io :as io]
+            [clojure.data.xml :as cdx]
+            [clojure.pprint :refer :all]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def transaction-summary "output/transaction-summary-%s.txt")
+
+(def transaction-content "output/transaction-content-%s.txt")
+
+
+(defn get-current-time
+  []
+  (.format (java.text.SimpleDateFormat. "yyyyMMddHHmm")
+           (java.util.Date.)))
+
+
+(defn indexed-transaction-content
+  [index transaction-content]
+  {index transaction-content})
+
+
+(defn xml->map
+  [xml-file]
+  (let [transaction-data (cdx/parse (java.io.StringReader. (slurp xml-file)))
+        transaction-summary (mapv indexed-transaction-content
+                                  (range)
+                                  (mapv :attrs (:content transaction-data)))
+        transaction-content (mapv indexed-transaction-content
+                                  (range)
+                                  (mapv :content
+                                        (:content transaction-data)))]
+    (binding [*out* (java.io.FileWriter. (format transaction-summary
+                                                 (get-current-time)))]
+      (pprint transaction-summary))
+    (binding [*out* (java.io.FileWriter. (format transaction-content
+                                                 (get-current-time)))]
+      (pprint transaction-content))))
